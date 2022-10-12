8abff9521d63ca60958065652e6eddde705854a8
Mellak <youness.mellak.96@gmail.com>
2022-10-12 10:59:49 +0200

    full_thing

---
+++ .history/CartPool_20221012101606.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision.transforms as T
+import gym
+
+
+
+
+
+env = gym.make('CartPole-v1')
+observation_space = env.observation_space.shape[0]
+action_space = env.action_space.n
+
+FC1_DIMS = 1024
+FC2_DIMS = 512
+LEARNING_RATE = 0.0001
+DEVICE = torch.device("cpu")
+
+class Network(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.input_shape = env.observation_space.shape
+        self.action_space = action_space
+
+        self.fc1 = nn.Linear(*self.input_shape, FC1_DIMS)
+        self.fc2 = nn.Linear(FC1_DIMS, FC2_DIMS)
+        self.fc3 = nn.Linear(FC2_DIMS, self.action_space)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=LEARNING_RATE)
+        self.loss = nn.MSELoss()
+        self.to(DEVICE)
+    
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+
+        return x  # returns the best action for us to take
